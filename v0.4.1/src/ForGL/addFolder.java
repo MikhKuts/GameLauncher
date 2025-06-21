39c7f7006380641e62c85a06b479f3a5e95eefab
MikhKuts <105879783+MikhKuts@users.noreply.github.com>
2025-06-21 20:49:59 +0300

    Исходники v0.4.1-beta
    Исходники v0.4.1-beta

---
+++ v0.4.1/src/ForGL/addFolder.java
@@ -0,0 +1,67 @@
+package ForGL;
+
+import java.io.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+public class addFolder extends JFrame{
+	
+	public ArrayList<Icon> iconFromGames = new ArrayList<Icon>();
+	private static ArrayList<Game> searchedGames = new ArrayList<Game>();
+	//private static ArrayList<File> foldersForSearch = new ArrayList<File>();
+	
+	public static void chooseFolder() {
+		searchedGames.clear();
+		DirectoryChooser folderChooser = new DirectoryChooser();
+		folderChooser.setTitle("Выбор директории");
+        
+        File folder = folderChooser.showDialog(null);
+        if (folder != new File("") || folder != null) {
+        	searchGames(folder);	
+        }else {
+        	
+        }
+        new windows.ChooseWindow(searchedGames);
+        main.Main.updateListOfGames();
+        
+	}
+	public static void chooseFile() {
+		searchedGames.clear();
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Выбор файла");
+        fileChooser.setSelectedExtensionFilter(new ExtensionFilter("Executable files", "*.exe"));
+        File file = fileChooser.showOpenDialog(null);
+        	if (file != null) {
+        	addGame(file);
+        	configManage.addListOfGames(searchedGames);
+        	main.Main.updateListOfGames();
+        }
+	}
+	
+	private static void searchGames(File dir) {
+		for(File item : dir.listFiles()){
+		    if(item.isDirectory()){
+		        searchGames(item);
+		    } else {
+		        if(item.getName().endsWith(".exe")) {
+		            addGame(item);
+		        }
+		    }
+		}
+	}
+
+	public static void addGame(File item) {
+		searchedGames.add(
+				new Game(
+						item.getName().substring(0, item.getName().length()-4),
+						item.toPath(), 
+						null
+					));
+	}
+	
+}
---
+++ v0.4.1/src/windows/ChooseWindow.java
@@ -0,0 +1,131 @@
+package windows;
+
+import java.util.ArrayList;
+
+import ForGL.Game;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.stage.*;
+
+
+public class ChooseWindow {
+	private ScrollPane scrollPane = new ScrollPane();
+	private AnchorPane scrollContent = new AnchorPane();
+	private VBox vbox = new VBox();
+	private Button finishButton = new Button("Finish");
+	private Button cancelButton = new Button("Cancel");
+	private CheckBox selectAllCheckBox = new CheckBox("Select all");
+	private Label instructionLabel = new Label("Select the file to be added.");
+	private ArrayList<CheckBox> selectGames = new ArrayList<CheckBox>();
+	private ArrayList<Game> games = new ArrayList<Game>();
+	
+	public ChooseWindow(ArrayList<Game> searchedGames) {
+		this.games = searchedGames;
+		initWinndow();
+	}
+
+	public void initWinndow() {
+		AnchorPane root = init();
+        root.setPrefHeight(400.0);
+        root.setPrefWidth(400.0);
+
+
+
+        Scene scene = new Scene(root);
+        Stage chooseWindow = new Stage();
+        chooseWindow.initModality(Modality.APPLICATION_MODAL);
+		chooseWindow.setWidth(435);
+		chooseWindow.setHeight(450);
+		chooseWindow.setResizable(false);
+		chooseWindow.centerOnScreen();
+		chooseWindow.show();
+        chooseWindow.setScene(scene);
+        chooseWindow.show();
+    }
+	
+	private AnchorPane init() {
+		scrollPane.setLayoutX(10.0);
+        scrollPane.setLayoutY(100.0);
+        scrollPane.setPrefHeight(250.0);
+        scrollPane.setPrefWidth(400.0);
+
+        scrollContent.setMinHeight(0.0);
+        scrollContent.setMinWidth(0.0);
+
+        //vbox.setPrefWidth(380.0);
+
+        for (Game game : games) {
+        	selectGames.add(checkBox(game));
+        	vbox.getChildren().add(selectGames.getLast());
+		}
+
+        scrollContent.getChildren().add(vbox);
+        scrollPane.setContent(scrollContent);
+
+        finishButton.setId("black-text");
+        finishButton.setLayoutX(323.0);
+        finishButton.setLayoutY(361.0);
+        finishButton.setMnemonicParsing(false);
+        finishButton.setPrefHeight(26.0);
+        finishButton.setPrefWidth(60.0);
+        finishButton.setOnAction(e -> {
+        	ArrayList<Game> temp = new ArrayList<Game>();
+        	for (int i = 0; i < vbox.getChildren().size(); i++) {
+        		if (selectGames.get(i).isSelected())
+        			temp.add(games.get(i));
+			}
+        	ForGL.configManage.addListOfGames(temp);
+        	main.Main.updateListOfGames();
+        	((Stage) (finishButton.getScene().getWindow())).close();
+        });
+
+        cancelButton.setId("black-text");
+        cancelButton.setLayoutX(263.0);
+        cancelButton.setLayoutY(361.0);
+        cancelButton.setMnemonicParsing(false);
+        cancelButton.setOnAction(e -> {
+        	((Stage) (cancelButton.getScene().getWindow())).close();
+        });
+
+        selectAllCheckBox.setId("black-text");
+        selectAllCheckBox.setLayoutX(14.0);
+        selectAllCheckBox.setLayoutY(365.0);
+        selectAllCheckBox.setMnemonicParsing(false);
+        selectAllCheckBox.setOnAction(e -> {
+        	for (CheckBox checkboxes : selectGames) {
+        		if (selectAllCheckBox.isSelected()) checkboxes.setSelected(true);
+        		else checkboxes.setSelected(false);
+			}
+        });
+
+        instructionLabel.setId("black-text");
+        instructionLabel.setLayoutX(14.0);
+        instructionLabel.setLayoutY(25.0);
+        instructionLabel.setPrefHeight(50.0);
+        instructionLabel.setPrefWidth(225.0);
+        instructionLabel.setFont(new Font(18.0));
+
+        AnchorPane temp = new AnchorPane();
+        temp.getChildren().addAll(
+            scrollPane,
+            finishButton,
+            cancelButton,
+            selectAllCheckBox,
+            instructionLabel
+        );
+		return temp;
+	}
+	
+	private CheckBox checkBox(Game game) {
+		CheckBox checkBox = new CheckBox(game.gamePath.toString());
+        checkBox.setMnemonicParsing(false);
+        checkBox.setPrefHeight(30.0);
+        checkBox.setMinWidth(400.0);
+        checkBox.setPadding(new Insets(0, 0, 0, 5.0));
+		return checkBox;
+	}
+	
+}
